3d9e7f95d9949f2bebc47ef31f92ddb7d633dc4c
ghizlane <ghizlane.el.adak@hva.nl>
2020-05-14 13:49:12 +0200

    activiteiten doorverwijzing

--- src/assets/js/controllers/activiteitenController.js
+++ src/assets/js/controllers/activiteitenController.js
@@ -14,59 +14,13 @@ class EventsController {
 
         $(".content").empty().append(this.eventsView);
 
-        this.eventsView.find(".btn").on("click", (event) => this.onAddEvent(event))
+        //wanneer er op de aanmaken knop wordt gedrukt
+        this.eventsView.find(".button").on("click", () => this.onAddPost());
     }
 
-    async onAddEvent(event) {
-        event.preventDefault();
-
-        //verzamelen van form gegevens
-        const name = this.eventsView.find("#inputName").val();
-        const personAmount = this.eventsView.find("#inputPersonAmount").val();
-        const eventDate = this.eventsView.find("#inputDate").val();
-        const description = this.eventsView.find("#inputDescription").val();
-        const place = this.eventsView.find("#inputLocation").val();
-        const eventBeginTime = this.eventsView.find("#inputBeginTime").val();
-        const eventEndTime = this.eventsView.find("#inputEndTime").val();
-
-        console.log(`${name} - ${personAmount} - ${eventDate} - ${description} - ${place} - ${eventBeginTime} ${eventEndTime}`);
-
-        if (name === "" || personAmount === "" || eventDate === null || description === "" || place === ""
-            || eventBeginTime === "" || eventBeginTime === null || eventEndTime === null || eventEndTime === "") {
-            alert("Vul alle velden in!")
-        } else if (personAmount < 2) {
-            alert("Aantal deelnemers mag niet kleiner zijn dan 2!")
-        } else if (personAmount > 50) {
-            alert("Aantal deelnemers mag niet groter zijn dan 50!")
-        } else if (eventBeginTime === eventEndTime){
-            alert("De begintijd mag niet gelijk zijn aan de eindtijd!")
-        } else if (eventBeginTime > eventEndTime){
-            alert("De eindtijd moet hoger zijn dan de begintijd! ")
-        }
-        else {
-
-            //versturen naar repository
-            try {
-                const eventId = await this.activiteitenRepository.create(name, personAmount, eventDate, description,
-                    place, eventBeginTime, eventEndTime);
-                console.log(eventId);
-                app.loadController(CONTROLLER_EVENTS)
-                alert("\nEvenement is geplaatst!")
-
-            } catch (e) {
-                if (e.code === 401) {
-                    this.eventsView
-                        .find(".error")
-                        .html(e.reason);
-                } else {
-                    console.log(e);
-                }
-            }
-        }
-    }
-
-    error() {
-        $(".content").html("Failed to load content")
+    //html pagina waar je een activiteit kunt aanmaken
+    onAddPost() {
+        new PostActiviteitenController();
     }
 
     async loadEvents() {
---
+++ src/assets/js/controllers/postActiviteitenController.js
@@ -0,0 +1,69 @@
+class PostActiviteitenController {
+
+    constructor() {
+        this.activiteitenRepository = new activiteitenRepository();
+        $.get("views/activiteitAanmaken.html")
+            .done((htmlData) => this.setup(htmlData))
+            .fail(() => this.error());
+    }
+
+    setup(htmlData) {
+        this.eventsView = $(htmlData);
+
+        $(".content").empty().append(this.eventsView);
+
+        this.eventsView.find(".btn").on("click", (event) => this.onAddEvent(event))
+    }
+
+    async onAddEvent(event) {
+        event.preventDefault();
+
+        //verzamelen van form gegevens
+        const name = this.eventsView.find("#inputName").val();
+        const personAmount = this.eventsView.find("#inputPersonAmount").val();
+        const eventDate = this.eventsView.find("#inputDate").val();
+        const description = this.eventsView.find("#inputDescription").val();
+        const place = this.eventsView.find("#inputLocation").val();
+        const eventBeginTime = this.eventsView.find("#inputBeginTime").val();
+        const eventEndTime = this.eventsView.find("#inputEndTime").val();
+
+        console.log(`${name} - ${personAmount} - ${eventDate} - ${description} - ${place} - ${eventBeginTime} ${eventEndTime}`);
+
+        if (name === "" || personAmount === "" || eventDate === null || description === "" || place === ""
+            || eventBeginTime === "" || eventBeginTime === null || eventEndTime === null || eventEndTime === "") {
+            alert("Vul alle velden in!")
+        } else if (personAmount < 2) {
+            alert("Aantal deelnemers mag niet kleiner zijn dan 2!")
+        } else if (personAmount > 50) {
+            alert("Aantal deelnemers mag niet groter zijn dan 50!")
+        } else if (eventBeginTime === eventEndTime){
+            alert("De begintijd mag niet gelijk zijn aan de eindtijd!")
+        } else if (eventBeginTime > eventEndTime){
+            alert("De eindtijd moet hoger zijn dan de begintijd! ")
+        }
+        else {
+
+            //versturen naar repository
+            try {
+                const eventId = await this.activiteitenRepository.create(name, personAmount, eventDate, description,
+                    place, eventBeginTime, eventEndTime);
+                console.log(eventId);
+                app.loadController(CONTROLLER_EVENTS);
+                alert("\nEvenement is geplaatst!")
+
+            } catch (e) {
+                if (e.code === 401) {
+                    this.eventsView
+                        .find(".error")
+                        .html(e.reason);
+                } else {
+                    console.log(e);
+                }
+            }
+        }
+    }
+
+    error() {
+        $(".content").html("Failed to load content")
+    }
+}
